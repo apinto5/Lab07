657e0c79307f20eb0d958cb5dd5e4706ca5ab8df
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-11-25 20:31:51 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init(){
+
+document.getElementById("entrybutton").addEventListener("click", buttonFunction);
+
+function buttonFunction(){
+	
+    var userInput = document.getElementById('entryinput').value
+    
+	document.getElementById('textoutput').innerHTML = userInput;
+    alert('Alexander: ' + input);
+}};
+
+window.addEventListener('load', init);
