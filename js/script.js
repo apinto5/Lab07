cb3aba1d3535c40dff9f139b5f5acd84e3bde6ba
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-11-25 20:52:36 -0500

    expanded alert text: line #8
    'Alexander: ' >> 'Alexander Pinto: '

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.getElementById("entrybutton").addEventListener("click", myFunction);
 function myFunction(){	
     var userInput = document.getElementById('entryinput').value
     document.getElementById('textoutput').innerHTML = userInput;
-    alert('Alexander: ' + userInput);
+    alert('Alexander Pinto: ' + userInput);
 }};
 
 window.addEventListener('load', init);
